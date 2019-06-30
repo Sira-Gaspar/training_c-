a6168487f9d80dace2e7d9124d2b0012652958bb
sogaspar <sogaspar@mts.ru>
2019-06-30 19:02:17 +0300

    Новая попытка поправить ошибку в тесте изменения групп (безуспешно)

--- addressbook_test_Sira/addressbook_test_Sira/Tests/GroupModificationTests.cs
+++ addressbook_test_Sira/addressbook_test_Sira/Tests/GroupModificationTests.cs
@@ -18,9 +18,14 @@ namespace addressbook_test_Sira
             newData.Header = "hli";
             newData.Footer = "hlfgh";
             List<GroupData> oldGroups = GroupData.GetAllFromDB();
-            app.Groups.Modify(oldGroups[6], newData);
+            app.Groups.Modify(oldGroups[8], newData);
+            Assert.AreEqual(oldGroups.Count, app.Groups.GetGroupsCount());
             List<GroupData> newGroups = GroupData.GetAllFromDB();
-            oldGroups[6].Name = newData.Name;
+            oldGroups[8].Name = newData.Name;
+            oldGroups[8].Header = newData.Header;
+            oldGroups[8].Footer = newData.Footer;
+            //GroupData.GetAllFromDB()[0].Name = newData.Name;
+
             oldGroups.Sort();
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
